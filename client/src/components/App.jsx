ea95a6b600ec43480d5aa7e8f790545f7ff964be
papat27 <pqpagba@gmail.com>
2020-02-03 16:07:20 -0500

    react, webpack, & client initialization

---
+++ client/src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1>{this.props.test}</h1>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  }
+};
+
+module.exports = config;
