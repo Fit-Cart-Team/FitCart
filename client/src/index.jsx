e43aa270b15e6ef41e97db87aae56d5997daf5d1
wjloo95 <loo.williamj@gmail.com>
2020-02-04 19:31:57 -0500

    Initialize react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(<App test="Test" />, document.getElementById('app'));
+ReactDOM.render(
+  <Router>
+    <App test="Test" />
+  </Router>,
+  document.getElementById('app')
+);
