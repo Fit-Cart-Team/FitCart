3bc9e4f72295cc075cbbc0d8aaae02bd922927dd
papat27 <pqpagba@gmail.com>
2020-02-05 19:51:47 -0500

    review tile initialization and prop drilling

--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import ReviewsList from './ReviewsList/ReviewsList';
 
 const RatingsReviews = () => {
   const { id } = useParams();
-  return <div>From Ratings & Reviews: {id}</div>;
+
+  const [reviewsList, setReviewsList] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://3.134.102.30/reviews/${id}/list`)
+      .then(( {data} ) => {
+        console.log(data);
+        setReviewsList(data.results);
+      })
+  }, []);
+
+  return (
+    <div>
+      <h1>
+        From Ratings & Reviews: {id}
+      </h1>
+      <ReviewsList id={id} reviewsList={reviewsList} />
+    </div>
+  );
 };
 
 export default RatingsReviews;
---
+++ client/src/components/RatingsReviews/ReviewsList/ReviewTile/RatingHelpfulness.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RatingHelpfulness = (props) => {
+
+  return (
+    <div>
+      <p>
+        Rating Helpfulness Component
+      </p>   
+    </div>
+  );
+};
+
+export default RatingHelpfulness;
