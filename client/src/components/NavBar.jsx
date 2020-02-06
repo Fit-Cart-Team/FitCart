679bb66a413823a8e7ad8d21a8b1287d2eb85563
wjloo95 <loo.williamj@gmail.com>
2020-02-06 00:18:09 -0500

    Add additional styling to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,11 +2,40 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div className="nav-bar">
-      <h1>HELLO NAVBAR</h1>
-      <input type="text"></input>
-      <button>Search</button>
-    </div>
+    // <div className="nav-bar">
+    // <img
+    //   src="https://cdn.iconscout.com/icon/premium/png-256-thumb/shopping-bags-8-859849.png"
+    //   className="logo"
+    // ></img>
+    // </div>
+    <ul>
+      <li>
+        <a href="/" className="active">
+          Home
+        </a>
+      </li>
+      <li>
+        <a href="/1">Onesie</a>
+      </li>
+      <li>
+        <a href="/2">Sunglasses</a>
+      </li>
+      <li>
+        <a href="/3">Joggers</a>
+      </li>
+      <li>
+        <a href="/4">Slacks</a>
+      </li>
+      <li>
+        <a href="/5">Shoes</a>
+      </li>
+      <li className="float">
+        <div className="search-bar">
+          <input type="text"></input>
+          <button>Search</button>
+        </div>
+      </li>
+    </ul>
   );
 };
 
