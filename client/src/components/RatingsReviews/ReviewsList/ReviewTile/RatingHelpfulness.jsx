c02570ed89208f9751812e0a27c2e8879405f01f
papat27 <pqpagba@gmail.com>
2020-02-07 13:38:48 -0500

    can report and mark helpful only once, helpfulness number increments when user clicks yes

--- client/src/components/RatingsReviews/ReviewsList/ReviewTile/RatingHelpfulness.jsx
+++ client/src/components/RatingsReviews/ReviewsList/ReviewTile/RatingHelpfulness.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 
 const RatingHelpfulness = (props) => {
   let review_id = props.reviewID;
 
+  const [markable, setMarkable] = useState(true);
+  const [reportable, setReportable] = useState(true);
+  const [helpfulness, setHelpfulness] = useState(props.helpfulness);
+
   const markHelpful = () => {
     Axios.put(`http://3.134.102.30/reviews/helpful/${review_id}`)
       .then((data) => {
-        console.log(data);
+        setMarkable(false);
+        setHelpfulness(helpfulness + 1);
       })
   }
 
   const sendReport = () => {
     Axios.put(`http://3.134.102.30/reviews/report/${review_id}`)
       .then((data) => {
-        console.log(data);
+        setReportable(false);
       })
   }
 
   return (
     <div>
       <span>Was this review helpful? </span>
-      <span onClick={markHelpful} >Yes</span>
-      <span>({props.helpfulness}) | </span>   
-      <span onClick={sendReport} >Report</span>
+      { markable ? <span onClick={markHelpful} >Yes</span> : <span>Yes</span> }
+      <span>({helpfulness}) | </span>   
+      { reportable ? <span onClick={sendReport} >Report</span> : <span>Reported</span> }
     </div>
   );
 };
