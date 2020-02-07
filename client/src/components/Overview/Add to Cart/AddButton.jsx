b3bb3208381ee8691b02db9c3f3dae9c03a0c787
wjloo95 <loo.williamj@gmail.com>
2020-02-07 15:54:49 -0500

    Update font for buttons

--- client/src/components/Overview/Add to Cart/AddButton.jsx
+++ client/src/components/Overview/Add to Cart/AddButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AddButton = () => {
   return (
     <button className="add-to-bag">
-      <span className="add-text">Add To Bag</span>
+      <span className="add-text">ADD TO BAG</span>
       <span className="fa fa-plus"></span>
     </button>
   );
