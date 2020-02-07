441ebd0246560435307a2cfa56949b51daf87b19
papat27 <pqpagba@gmail.com>
2020-02-07 13:39:20 -0500

    renders stars accordingly

--- client/src/components/RatingsReviews/ReviewsList/ReviewTile/StarRating.jsx
+++ client/src/components/RatingsReviews/ReviewsList/ReviewTile/StarRating.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const StarRating = (props) => {
+  const starVal = (Math.round(props.rating * 4) / 4).toFixed(2);
+  return <div className="stars" style={{ '--rating': starVal }}></div>;
 
   return (
     <span>Star Rating: {props.rating}</span>
