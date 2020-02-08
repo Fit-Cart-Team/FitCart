a691c1e74718cc2c53e93d769dd1c4f0b70cbdd2
papat27 <pqpagba@gmail.com>
2020-02-08 15:04:42 -0500

    intialized other components and began logic for data to be given to Overview widget

---
+++ client/src/components/RatingsReviews/RatingBreakdown/RatingBreakdown.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RatingBreakdown = (props) => {
+
+  return (
+    <div>
+      <h2>Rating Breakdown Component</h2>
+    </div>
+  );
+}
+
+export default RatingBreakdown;
--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -3,29 +3,96 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import ReviewsList from './ReviewsList/ReviewsList';
 
-const RatingsReviews = () => {
+//setAppAvg(value)
+//setAppTotal(value)
+
+// Meta Data:
+//
+// {
+//   "product_id": "2",
+//   "ratings": {
+//     2: 1,
+//     3: 1,
+//     4: 2,
+//     // ...
+//   },
+//   "recommended": {
+//     0: 5
+//     // ...
+//   },
+//   "characteristics": {
+//     "Size": {
+//       "id": 14,
+//       "value": "4.0000"
+//     },
+//     "Width": {
+//       "id": 15,
+//       "value": "3.5000"
+//     },
+//     "Comfort": {
+//       "id": 16,
+//       "value": "4.0000"
+//     },
+//     // ...
+// }
+
+const RatingsReviews = (props) => {
   const { id } = useParams();
 
   const [url, seturl] = useState(id);
   const [reviewsList, setReviewsList] = useState([]);
+  const [meta, setMeta] = useState({});
+  const [totalRatings, setTotalRatings] = useState(0);
+  const [ratingAvg, setRatingAvg] = useState(0);
 
   if (url !== id) {
     seturl(id);
   }
 
   useEffect(() => {
-    axios.get(`http://3.134.102.30/reviews/${url}/list`)
+    axios.get(`http://3.134.102.30/reviews/${url}/meta`)
       .then(( {data} ) => {
-        setReviewsList(data.results);
+        console.log(data);
+        setMeta(data);
+        return data;
+      })
+      .then((data) => {
+        let totalQuantity = 0;
+        let ratingSum = 0;
+
+        console.log(data);
+
+        for (let rating in data.ratings) {
+          console.log('Rating: ', rating)
+          totalQuantity += data.ratings[rating];
+          ratingSum += rating * data.ratings[rating];
+        }
+
+        console.log('Total quantity of ratings: ', totalQuantity)
+
+        setTotalRatings(totalQuantity);
+
+        let ratingAvg = ratingSum / totalQuantity;
+
+        console.log('Rating Average: ', ratingAvg);
+        
+        setRatingAvg(ratingAvg);
       })
   }, [url]);
+
+  // useEffect(() => {
+  //   axios.get(`http://3.134.102.30/reviews/${url}/list`)
+  //     .then(( {data} ) => {
+  //       setReviewsList(data.results);
+  //     })
+  // }, [url]);
 
   return (
     <div id="ratings-reviews" >
       <h1>
         Ratings & Reviews
       </h1>
-      <ReviewsList id={id} reviewsList={reviewsList} />
+      {/* <ReviewsList id={id} reviewsList={reviewsList} /> */}
     </div>
   );
 };
