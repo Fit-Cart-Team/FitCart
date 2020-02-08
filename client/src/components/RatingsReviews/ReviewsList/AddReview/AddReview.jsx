52e1b79c286120431f978e1ae8a2f66b26fb2202
papat27 <pqpagba@gmail.com>
2020-02-08 11:33:54 -0500

    Initialized button - no functionality

---
+++ client/src/components/RatingsReviews/ReviewsList/AddReview/AddReview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddReview = (props) => {
+
+  return (
+    <span>
+      <button>Add a Review Component</button>
+    </span>
+  );
+}
+
+export default AddReview;
---
+++ client/src/components/RatingsReviews/ReviewsList/MoreReviews/MoreReviews.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MoreReviews = (props) => {
+
+  return (
+    <span>
+      <button>More Reviews Component</button>
+    </span>
+  );
+}
+
+export default MoreReviews;
