9c49a44f106341371ceedfb92a39cff82a48b153
Erin Levsen <erinlevsen@Erins-MacBook-Pro.local>
2020-02-10 17:02:24 -0500

    semantic ui install, photo modal

---
+++ client/src/components/QA/Answers/PhotoModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Divider, Header, Image, Modal } from 'semantic-ui-react';
+
+const PhotoModal = ({ photo }) => {
+  let style = {
+    margin: '20px',
+    width: '5rem',
+    height: '4rem',
+  };
+  return (
+    <Modal trigger={<img src={photo.url} style={style} id={photo.id}></img>}>
+      <Modal.Content image>
+        <Image fluid src={photo.url} />
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default PhotoModal;
