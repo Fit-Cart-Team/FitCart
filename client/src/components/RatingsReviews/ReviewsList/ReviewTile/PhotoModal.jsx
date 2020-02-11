71f65574885d9e083bc2049cca87e9b2978f12ea
papat27 <pqpagba@gmail.com>
2020-02-11 14:39:00 -0500

    implemented photo modal

---
+++ client/src/components/RatingsReviews/ReviewsList/ReviewTile/PhotoModal.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button, Icon, Image, Modal } from 'semantic-ui-react'
+
+const PhotoModal = (props) => {
+  let style = {
+    margin: "10px",
+    width: "100px",
+    height: "100px",
+    "cursor": "pointer"
+  };
+  
+  return (
+    <Modal trigger={<img src={props.photo.url} style={style} ></img>} basic closeIcon >
+      <Modal.Content image>
+        <Image src={props.photo.url} size="big" verticalAlign="top" centered rounded />
+      </Modal.Content>
+    </Modal>
+  );
+}
+
+export default PhotoModal;
