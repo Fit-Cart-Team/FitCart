2cf2d2a02e58c9165bbcf9a3dd078c3020cb9f32
wjloo95 <loo.williamj@gmail.com>
2020-02-11 15:05:46 -0500

    Adjust related products render/images

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -16,35 +16,40 @@ const RelatedProducts = ({ prodInfo, styleInfo }) => {
   }
 
   useEffect(() => {
-    setrelatedProds([]);
-    setrelatedStyles([]);
     axios.get(`http://3.134.102.30/products/${url}/related`).then(results => {
-      Promise.all(
-        results.data.map(product => {
-          return axios
-            .get(`http://3.134.102.30/products/${product}`)
-            .then(results => {
-              setrelatedProds(prev => [...prev, results.data]);
-            })
-            .then(() => {
-              axios
-                .get(`http://3.134.102.30/products/${product}/styles`)
-                .then(results => {
-                  let styles = results.data.results;
-                  let needStyle = true;
-                  styles.forEach(style => {
-                    if (style['default?'] === 1) {
-                      setrelatedStyles(prev => [...prev, style]);
-                      needStyle = false;
-                    }
-                  });
-                  if (needStyle) {
-                    setrelatedStyles(prev => [...prev, styles[0]]);
-                  }
-                });
+      // Get all the product information for each related product
+      const prodPromises = [];
+      results.data.forEach(product => {
+        prodPromises.push(axios.get(`http://3.134.102.30/products/${product}`));
+      });
+
+      // Get all the style information for each related product
+      const stylePromises = [];
+      results.data.forEach(product => {
+        stylePromises.push(
+          axios.get(`http://3.134.102.30/products/${product}/styles`)
+        );
+      });
+
+      Promise.all(prodPromises).then(products => {
+        // Set state with each product's information
+        const prodData = products.map(prod => prod.data);
+        setrelatedProds(prodData);
+        Promise.all(stylePromises).then(styles => {
+          // Get default style for each product and set state
+          const tempStyleData = styles.map(style => style.data);
+          const styleData = tempStyleData.map(style => {
+            let styleResults = style.results;
+            styleResults.forEach(style => {
+              if (style['default?'] === 1) {
+                return style;
+              }
             });
-        })
-      );
+            return styleResults[0];
+          });
+          setrelatedStyles(styleData);
+        });
+      });
     });
   }, [url]);
 
