bcb6699eaae3b146ae4ca350aac266b94b0f2214
papat27 <pqpagba@gmail.com>
2020-02-13 02:27:54 -0500

    5/8 form functionality

--- client/src/components/RatingsReviews/ReviewsList/AddReview/AddReviewForm.jsx
+++ client/src/components/RatingsReviews/ReviewsList/AddReview/AddReviewForm.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useFormik } from 'formik';
 
 const validate = values => {
   const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  } else if (values.firstName.length > 15) {
-    errors.firstName = 'Must be 15 characters or less';
+  if (values.summary.length > 60) {
+    errors.summary = 'Must be 60 characters or less';
   }
 
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  } else if (values.lastName.length > 20) {
-    errors.lastName = 'Must be 20 characters or less';
+  if (!values.body) {
+    errors.body = 'Required';
+  } else if (values.body.length > 1000) {
+    errors.body = 'Must be 1000 characters or less';
+  } else if (values.body.length < 50) {
+    errors.body = 'Must be 50 characters or more';
+  }
+
+  if (!values.name) {
+    errors.name = 'Required';
+  } else if (values.name.length > 60) {
+    errors.name = 'Must be 60 characters or less';
   }
 
   if (!values.email) {
@@ -25,55 +31,118 @@ const validate = values => {
 };
 
 const AddReviewForm = (props) => {
+  const { chars } = props;
+
+  const [recommend, setRecommend] = useState(false);
+
+  const handleRecommendClick = (e) => {
+    if (e.target.value === 'true') {
+      setRecommend(true);
+    } else {
+      setRecommend(false);
+    }
+  }
+
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
+      summary: '',
+      body: '',
+      name: '',
+      email: ''
     },
     validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: formikValues => {
+      let stateInputs = {
+        recommend: recommend
+      }
+
+      alert(JSON.stringify(Object.assign(formikValues, stateInputs), null, 2));
     },
   });
+
   return (
     <form onSubmit={formik.handleSubmit}>
       <div>
-        <label htmlFor="firstName">First Name</label>
+        <label htmlFor="recommend">Do you recommend this product?*</label>
         <input
-          id="firstName"
-          name="firstName"
+          id="recommend"
+          name="recommend"
+          type="radio"
+          value="true"
+          onClick={handleRecommendClick}
+        />Yes
+        <input
+          id="recommend"
+          name="recommend"
+          type="radio"
+          value="false"
+          onClick={handleRecommendClick}
+          checked
+        />No
+      </div>
+      <div>
+        <label htmlFor="summary">Review Summary</label>
+        <input
+          id="summary"
+          name="summary"
           type="text"
+          placeholder="Example: Best purchase ever!"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
-          value={formik.values.firstName}
+          value={formik.values.summary}
         />
+        {formik.touched.summary && formik.errors.summary ? (
+          <div>{formik.errors.summary}</div>
+        ) : null}
       </div>
-      {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
       <div>
-        <label htmlFor="lastName">Last Name</label>
+        <label htmlFor="body">Review Body*</label>
         <input
-          id="lastName"
-          name="lastName"
+          id="body"
+          name="body"
           type="text"
+          placeholder="Why did you like the product or not?"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
-          value={formik.values.lastName}
+          value={formik.values.body}
         />
+        <div>{ (formik.values.body.length < 50) ? (`Minimum required characters left: ${50 - formik.values.body.length}`) : ('Minimum reached') }</div>
+        {formik.touched.body && formik.errors.body ? (
+          <div>{formik.errors.body}</div>
+        ) : null}
       </div>
-      {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
       <div>
-        <label htmlFor="email">Email Address</label>
+        <label htmlFor="name">What is your nickname*</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          placeholder="Example: jackson11"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+        <div>For privacy reasons, do not use your full name or email address</div>
+        {formik.touched.name && formik.errors.name ? (
+          <div>{formik.errors.name}</div>
+        ) : null}
+      </div>
+      <div>
+        <label htmlFor="email">Your email*</label>
         <input
           id="email"
           name="email"
           type="email"
+          placeholder="Example: jackson11@email.com"
           onChange={formik.handleChange}
           onBlur={formik.handleBlur}
           value={formik.values.email}
         />
+        <div>For authentication reasons, you will not be emailed</div>
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
       </div>
-      {formik.errors.email ? <div>{formik.errors.email}</div> : null}
       <button type="submit">Submit</button>
     </form>
   );
--- client/src/components/RatingsReviews/ReviewsList/AddReview/ReviewModal.jsx
+++ client/src/components/RatingsReviews/ReviewsList/AddReview/ReviewModal.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import AddReviewForm from './AddReviewForm';
 import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
-const ReviewModal = () => (
+const ReviewModal = (props) => {
+  const { characteristics } = props;
+
+  return (
   <Modal trigger={<Button>Add a Review</Button>} >
     <Header icon='plus square outline' content='Add a review about the [product name]' />
     <Modal.Content>
-      <AddReviewForm />
+      <AddReviewForm chars={characteristics} />
     </Modal.Content>
     <Modal.Actions>
       <Button color='green' inverted>
@@ -14,6 +17,7 @@ const ReviewModal = () => (
       </Button>
     </Modal.Actions>
   </Modal>
-)
+  );
+}
 
 export default ReviewModal;
