5c5865894dbbd2f3a6de884d1f53d2406f140556
papat27 <pqpagba@gmail.com>
2020-02-12 23:56:35 -0500

    exploring formik

--- client/src/components/RatingsReviews/ReviewsList/AddReview/ReviewModal.jsx
+++ client/src/components/RatingsReviews/ReviewsList/AddReview/ReviewModal.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import AddReviewForm from './AddReviewForm';
 import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
 const ReviewModal = () => (
   <Modal trigger={<Button>Add a Review</Button>} >
-    <Header icon='plus square outline' content='Add a review' />
+    <Header icon='plus square outline' content='Add a review about the [product name]' />
     <Modal.Content>
-      <p>
-        Hello
-      </p>
+      <AddReviewForm />
     </Modal.Content>
     <Modal.Actions>
       <Button color='green' inverted>
